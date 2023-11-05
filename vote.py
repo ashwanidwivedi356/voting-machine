e6e3df483169909bcc3ad85ca9ecc9e5b0b0da48
Ashwani Kumar Dwivedi <103239473+ashwanidwivedi356@users.noreply.github.com>
2023-11-05 20:08:49 +0530

    Add files via upload

---
+++ vote.py
@@ -0,0 +1,36 @@
+#
+
+
+
+
+
+n=int(input("number of voter :"))
+voters_name=[]
+voters_age=[]
+for i in range(n):
+    voters_name.append(input("Voters Name :"))
+    voters_age.append(int(input("Voters Age :")))
+voters=dict(zip(voters_age,voters_name))
+for i,j in list(voters.items()):
+    if i<18:
+        voters.pop(i)
+candiate_1=input("Candidate First Name :")
+candiate_2=input("Candidate Second Name :")
+c_1=0
+c_2=0
+for i,j in voters.items():
+    vote=input("Enter The Candiate Name Whom You Wanted To Voted :")
+    if vote==candiate_1:
+        c_1+=1
+    elif vote==candiate_2:
+        c_2+=1
+    else:
+        pass 
+if c_1>c_2:
+    print(candiate_1,"is winner")
+elif c_2>c_1:
+    print(candiate_2,"is winner")
+elif c_1==c_2:
+    print("Draw")
+else:
+    print("No Result")
